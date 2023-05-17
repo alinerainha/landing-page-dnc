3d3f561a6c43a3bbfa5063062d534c1c41e8fb8a
alinerainha <aline_rainha@hotmail.com>
2023-05-16 21:32:21 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var setaDireita = window.document.getElementById("seta-direita")
+
+function rolarParaDireita() {
+    leonardo.style = "display: none"
+    bruna.style = "display: flex"
+    setaDireita.style = "display: none"
+    setaEsquerda.style = "display: flex"
+}
+
+function rolarParaEsquerda(){
+    leonardo.style = "display: flex"
+    bruna.style = "display: none"
+    setaDireita.style = "display: flex"
+    setaEsquerda.style = "display: none"
+}
